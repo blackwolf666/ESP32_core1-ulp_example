fe8cca1202e8c6588d2e55447cf8f1bfd989e3eb
blackwolf666 <therealblackwolf@gmail.com>
2020-12-31 09:28:20 +0100

    Add files via upload
    Added two task, one per core, plus ULP

--- main/ulp.c
+++ main/ulp.c
@@ -11,15 +11,61 @@
 #define _MYWAIT     __MYWAIT,\
                     __MYWAIT
 
-/* 16x 65535 it's enough */
+/* 16x 65535 is enough */
 #define MYWAIT      _MYWAIT,\
                     _MYWAIT,\
                     _MYWAIT,\
                     _MYWAIT
 
 
+void task0(void* params){
+    int core=xPortGetCoreID();
+    printf("Task starting on core %d\n",core);
+
+    int i=*((int*)params);
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = BIT(i);
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    int s = 1;
+    while(1){
+        s=1-s;
+        vTaskDelay(750 / portTICK_PERIOD_MS);
+        gpio_set_level(i, s);
+    }
+}
+
+void task1(void* params){
+    int core=xPortGetCoreID();
+    printf("Task starting on core %d\n",core);
+
+    int i=*((int*)params);
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = BIT(i);
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    int s = 1;
+    for (;;) {
+        s = 1-s;
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        gpio_set_level(i, s);
+    }
+
+}
 
 void app_main(void) {
+    int core=xPortGetCoreID();
+    int pin0=0,pin1=4;
+    printf("app_main starting on core %d\n",core);
+
     ulp_insn_t program[] = {
         I_WR_REG(RTC_IO_TOUCH_PAD2_REG, RTC_IO_TOUCH_PAD2_MUX_SEL_S,RTC_IO_TOUCH_PAD2_MUX_SEL_S, 1),
         I_WR_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + 12,  RTC_GPIO_OUT_DATA_S + 12, 1),
@@ -31,26 +77,15 @@ void app_main(void) {
         M_BX(1)
     };
 
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = GPIO_SEL_4;
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-
-    vTaskDelay(1000/portTICK_PERIOD_MS);
 
     size_t load_addr = 0;
     size_t size = sizeof(program)/sizeof(ulp_insn_t);
     ulp_process_macros_and_load(load_addr, program, &size);
     ulp_run(load_addr);
+    printf("Blinking onboard GPIO[2] on ULP\n");
 
-    int s = 1;
-    for (;;) {
-        s = 1-s;
-        gpio_set_level(4, s);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
-
+    xTaskCreatePinnedToCore(task0,"my_task0",10000,&pin0,1,NULL,0);
+    xTaskCreatePinnedToCore(task1,"my_task1",10000,&pin1,1,NULL,1);
+    while(1) {    vTaskDelay(1000/portTICK_PERIOD_MS);
+}
 }
